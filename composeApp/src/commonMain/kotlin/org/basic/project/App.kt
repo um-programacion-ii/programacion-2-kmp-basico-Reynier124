7a4474d37b4335c0761625132bb668bff4beaa05
Reynier124 <reynier1306@gmail.com>
2025-09-02 17:46:40 -0300

    Cuarto push

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -31,6 +31,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import kmpbasico.composeapp.generated.resources.Res
 import kmpbasico.composeapp.generated.resources.compose_multiplatform
+import org.basic.project.bottonBar.BottomBarScreen
 
 @Composable
 fun App() {
@@ -116,6 +117,21 @@ class MainScreen: Screen {
                         text = "Ir a la tercera página"
                     )
                 }
+            }
+
+            Spacer(
+                modifier = Modifier.height(30.dp)
+            )
+            Button(
+                onClick = {
+                    navigator.push(
+                        item = BottomBarScreen()
+                    )
+                }
+            ){
+                Text(
+                    text = "BottomBar"
+                )
             }
 
         }
